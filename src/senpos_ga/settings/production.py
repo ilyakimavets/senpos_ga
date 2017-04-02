651a186257a940b7cb91dc7064aede72b35b6835
Kovalchuk Evgeny <kovalchuk.evgeny@gmail.com>
2017-04-02 11:45:42 +0300

    updated requirement.txt and settings for postgresql

--- src/senpos_ga/settings/production.py
+++ src/senpos_ga/settings/production.py
@@ -1,4 +1,11 @@
 from .base import *
 
+import dj_database_url
+
+
 ALLOWED_HOSTS += ['senpos-ga.herokuapp.com']
 DEBUG = False
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
