b183974dbf9aa95f2433273b869e906993eb4333
Kovalchuk Evgeny <kovalchuk.evgeny@gmail.com>
2017-04-02 16:01:06 +0300

    fixed static and related

--- src/senpos_ga/settings/base.py
+++ src/senpos_ga/settings/base.py
@@ -37,13 +37,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'senpos_ga.urls'
@@ -108,14 +108,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- src/senpos_ga/settings/production.py
+++ src/senpos_ga/settings/production.py
@@ -3,19 +3,13 @@ from .base import *
 import dj_database_url
 
 
-ALLOWED_HOSTS += [
-    'senpos-ga.herokuapp.com'
-]
-
-# static files handle
-# MIDDLEWARE += [
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
+# ALLOWED_HOSTS += [
+#     'senpos-ga.herokuapp.com'
 # ]
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+ALLOWED_HOSTS += ['*']
 
 DEBUG = False
 
-# Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
